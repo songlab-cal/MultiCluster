f4b67d2a15460e5422d099eb5736d6f0e8069183
Wang lab <saplingwang@gmail.com>
2019-05-03 01:17:50 -0500

    update

--- software/demo.m
+++ software/demo.m
@@ -1,27 +1,17 @@
-% creat an order-3 tensor
-d1=10;
-d2=20;
-d3=100;
+%% creat an order-3 tensor
+rand('state',0);
 
-v1 = rand(1,d1);
-v2 = rand(1,d2);
-v3 = rand(1,d3);
-
-v1=v1/norm(v1);
-v2=v2/norm(v2);
-v3=v3/norm(v3);
-
-noise=0.001;
-Ncomp=2;
-%
-Tensor=reshape(kron(kron(v3,v2),v1),[d1,d2,d3])+noise*reshape(rand(1,d1*d2,d3),[d1,d2,d3]);  
+A = randn(3,2); %<-- First column is a_1, second is a_2.
+B = randn(4,2); %<-- Likewise for B.
+C = randn(5,2); %<-- Likewise for C.
+C(C<=0)=0
+Tensor =ktensor({A,B,C}); %<-- Create the ktensor.
+Tensor=full(Tensor);
+missing=zeros([3,4,5])+1;
 
 % perform tensor decomposition with semi-nonnegative constraint in the Z-mode
 
-[output_vector_X,output_vector_Y,output_vector_Z,output_value]=MultiCluster(Tensor,Ncomp);
+[output_vector_X,output_vector_Y,output_vector_Z,output_value]=HOSVD_positive(Tensor,10,missing);
 
 % output
-output_vector_X(:,1)
-output_vector_Y(:,1)
-output_vector_Z(:,1)
-output_value
+output_vector_X
